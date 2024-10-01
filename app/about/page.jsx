23c9a09e62a45e89b03d7889530c2180ee04da84
PABLOGONZALEZCUADRADO <pablogonzalezcuadrado@hotmail.com>
2024-10-01 17:46:20 +0200

    otra vez mas paginas
    -otra vez mas paginas

---
+++ app/about/page.jsx
@@ -0,0 +1,8 @@
+export default function AboutPage () {
+    return (
+        <>
+        <h1>hollow</h1>
+        <p>su reviews</p>
+        </>
+    );
+}
---
+++ app/reviews/hollow/page.jsx
@@ -0,0 +1,8 @@
+export default function HollowPage () {
+    return (
+        <>
+        <h1>hollow</h1>
+        <p>su reviews</p>
+        </>
+    );
+}
