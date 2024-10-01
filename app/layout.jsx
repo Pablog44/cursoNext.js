cfcc7ba635ef3b3533e05962f523ca4b69bbe415
PABLOGONZALEZCUADRADO <pablogonzalezcuadrado@hotmail.com>
2024-10-01 17:22:28 +0200

    pagina y layout
    -pagina y layout

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,7 +3,15 @@ export default function RootLayoutf({ children }) {
     return (
     <html lang="en">
         <body>
+            <header>
+                [header]
+            </header>
+            <main>
             { children }
+            </main>
+            <footer>
+                [footer]
+            </footer>
         </body>
 
     </html>
