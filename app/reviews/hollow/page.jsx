aa79b47abf970454ff8358e47e8cd5f058195d55
PABLOGONZALEZCUADRADO <pablogonzalezcuadrado@hotmail.com>
2024-10-03 18:16:05 +0200

    carpeta publica
    -para imagenes o lo que haga falta.

--- app/reviews/hollow/page.jsx
+++ app/reviews/hollow/page.jsx
@@ -4,6 +4,9 @@ export default function HollowPage () {
     return (
         <>
         <Heading>hollow</Heading>
+        <img src="/hollow-knight.jpg" alt="" 
+        with="640" height="360" className='w-1/2 mb-2 rounded-2xl'
+        />
         <p>su reviews</p>
         </>
     );
--- app/reviews/stardew/page.jsx
+++ app/reviews/stardew/page.jsx
@@ -4,6 +4,9 @@ export default function ReviewsPage () {
     return (
         <>
         <Heading>Stardew</Heading>
+        <img src="/stardew-valley.jpg" alt="" 
+        with="640" height="360" className='w-1/2 mb-2 rounded-2xl'
+        />
         <p>su reviews</p>
         </>
     );
