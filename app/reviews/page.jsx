084e8ef3230179c79ebe8b43d8bbd049f4dc4427
PABLOGONZALEZCUADRADO <pablogonzalezcuadrado@hotmail.com>
2024-10-04 12:16:50 +0200

    usar imagenes con diferentes estilos
    -se usan imágenes con diferentes estilos dentro de links como tarjetas

--- app/reviews/page.jsx
+++ app/reviews/page.jsx
@@ -5,12 +5,20 @@ export default function ReviewsPage () {
     return (
         <>
         <Heading>Reviews</Heading>
-        <ul>
-            <li>
-                <Link href="/reviews/hollow">Hollow Knight</Link>
+        <ul className="fex flex-col gap-3 mb-2">
+            <li className="bg-white hover:bg-orange-800 border rounded shadow hover:shadow-2xl w-80 mt-2">
+                <Link href="/reviews/hollow">
+                <img src="/hollow-knight.jpg" alt="" 
+                with="640" height="360" className='rounded-t'
+                />
+                <h2 className="text-center py-1">Hollow Knight</h2></Link>
             </li>
-            <li>
-                <Link href="/reviews/stardew">Stardew Valley</Link>
+            <li className="bg-white hover:bg-orange-800 border rounded shadow hover:shadow-2xl w-80 mt-3">
+                <Link href="/reviews/hollow">
+                <img src="/stardew-valley.jpg" alt="" 
+                with="640" height="360" className='rounded-t'
+                />
+                <h2 className="text-center py-1">Stardew Valley</h2></Link>
             </li>
         </ul>
         </>
