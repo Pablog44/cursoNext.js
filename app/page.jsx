e263f2b964bbda18c92d999e089b268a9d3b96d3
PABLO GONZALEZ CUADRADO <pablogonzalezcuadrado@hotmail.com>
2024-10-06 00:22:01 +0200

    cogemos los datos de la msima forma para ponerlos el home page
    -cogemos los datos de la msima forma para ponerlos el home page

--- app/page.jsx
+++ app/page.jsx
@@ -1,20 +1,22 @@
 import Link from 'next/link';
 import Heading from '../components/heading';
+import {getFeaturedReview} from '@/lib/reviews';
 
-export default function HomePage () {
+export default async function HomePage() {
+    const review = await getFeaturedReview();
     console.log('[HomePage] rendering');
-        return(
+    return (
         <>
-        <Heading>My next.js page</Heading>
-        <p className='pb-3'>only greatest games</p>
-        <div className="bg-white hover:bg-orange-800 border rounded shadow hover:shadow-2xl w-80 mt-3 sm:w-full">
-                <Link href="/reviews/hollow"
-                className='flex flex-col sm:flex-row'
+            <Heading>My next.js page</Heading>
+            <p className='pb-3'>only greatest games</p>
+            <div className="bg-white hover:bg-orange-800 border rounded shadow hover:shadow-2xl w-80 mt-3 sm:w-full">
+                <Link href={`/reviews/${review.slug}`}
+                    className='flex flex-col sm:flex-row'
                 >
-                <img src="/stardew-valley.jpg" alt="" 
-                with="320" height="180" className='rounded-t sm:rounded-l sm:rounded-r-none'
-                />
-                <h2 className="font-orbitron font-semibold text-center py-1 sm:px-2">Stardew Valley</h2></Link>
+                    <img src= {review.image} alt=""
+                        with="320" height="180" className='rounded-t sm:rounded-l sm:rounded-r-none'
+                    />
+                    <h2 className="font-orbitron font-semibold text-center py-1 sm:px-2">{review.title}</h2></Link>
             </div>
         </>
     );
