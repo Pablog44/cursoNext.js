4b6e03bb6d07c845d3ab5b09f5794de4fd379709
PABLO GONZALEZ CUADRADO <pablogonzalezcuadrado@hotmail.com>
2024-10-05 14:11:02 +0200

    Codigo mas limpio separando data en funcion y metiendolo en lib
    -Codigo mas limpio separando data en funcion y metiendolo en lib

--- app/reviews/stardew/page.jsx
+++ app/reviews/stardew/page.jsx
@@ -1,24 +1,21 @@
-import { readFile } from 'node:fs/promises';
+
 import Heading from '@/components/heading';
-import { marked } from 'marked';
-import matter from 'gray-matter';
-
-export default async function ReviewsPage () {
-    const text = await readFile('./content/reviews/stardew-valley.md', 'utf8');
-    const { content, data: { title, date, image } } = matter(text);
-    const html = marked(content, {headerIds: false, mangle: false});
+import { getReview } from '@/lib/reviews';
 
 
+
+export default async function ReviewsPage(slug) {
+    const review = await getReview('stardew-valley');
     return (
         <>
-        <Heading>{title}</Heading>
-        <p className='italic pb-1'>{date}</p>
-        <img src={image} alt="" 
-        with="640" height="360" className='w-1/2 mb-2 rounded-2xl'
-        />
-        <article dangerouslySetInnerHTML={{ __html: html }} 
-        className='max-w-screen-sm prose prose-slate'
-        />
+            <Heading>{review.title}</Heading>
+            <p className='italic pb-1'>{review.date}</p>
+            <img src={review.image} alt=""
+                with="640" height="360" className='w-1/2 mb-2 rounded-2xl'
+            />
+            <article dangerouslySetInnerHTML={{ __html: review.body }}
+                className='max-w-screen-sm prose prose-slate'
+            />
         </>
     );
 }
