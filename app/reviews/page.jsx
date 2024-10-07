69f67dc9225074b7be57a37164779537c555fe68
PABLO GONZALEZ CUADRADO <pablogonzalezcuadrado@hotmail.com>
2024-10-07 02:03:29 +0200

    seguimos cargando la api de strapi
    -seguimos cargando la api de strapi

--- app/reviews/page.jsx
+++ app/reviews/page.jsx
@@ -7,8 +7,8 @@ export const metadata = {
 }
 
 export default async function ReviewsPage() {
-    []
     const reviews = await getReviews();
+    console.log('[ReviewsPage] reviews:', reviews);
 
     return (
         <>
